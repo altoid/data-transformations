7da563185f7153881f13f7efaa638f56d6f78f51
Doug Tomm <dtomm@radiumone.com>
2019-08-29 21:07:10 -0700

    changes to make this work under intellij

--- src/main/scala/main/HelloSpark.scala
+++ src/main/scala/main/HelloSpark.scala
@@ -4,7 +4,9 @@ import org.apache.spark.sql.SparkSession
 
 object HelloSpark {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder.appName("Hello Spark App").getOrCreate()
+    val spark = SparkSession.builder.appName("Hello Spark App")
+      .master("local[*]")  // had to add this
+      .getOrCreate()
     println("Hello Spark")
     spark.stop()
   }
